bb0daa51180fb5e50ebcf5e50ed6d362f58441ee
DATDQ1510 <datdq5275@gmail.com>
2024-12-28 21:19:57 +0700

    them phi co so

--- src/app.js
+++ src/app.js
@@ -69,8 +69,8 @@ app.use('/bill', (req, res) => {
     res.render('bills/bill')
 })
 
-app.use('/tra_cuu', (req, res) => {
-    res.render('tra_cuu')
+app.use('/phi_co_so', (req, res) => {
+    res.render('phi_co_so')
 })
 
 app.get('/resident', (req, res) => {
